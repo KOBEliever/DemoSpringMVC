d6a117e5ab81a154900d78a920c1526dc8cff373
一 丁 <970102437@qq.com>
2022-06-19 22:26:23 +0800

    接口测试

--- SpringMVCssm/src/main/java/com/cqu/config/JdbcConfig.java
+++ SpringMVCssm/src/main/java/com/cqu/config/JdbcConfig.java
@@ -7,18 +7,20 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+
 public class JdbcConfig {
-    @Value("$com.mysql.cj.jdbc.Driver")
+    @Value("${jdbc.driver}")
     private String driver;
-    @Value("jdbc:mysql://localhost:3306/ssm_db")
+    @Value("${jdbc.url}")
     private String url;
-    @Value("root")
+    @Value("${jdbc.username}")
     private String username;
-    @Value("010609dy")
+    @Value("${jdbc.password}")
     private String password;
+
     @Bean
     public DataSource dataSource(){
-        DruidDataSource dataSource=new DruidDataSource();
+        DruidDataSource dataSource = new DruidDataSource();
         dataSource.setDriverClassName(driver);
         dataSource.setUrl(url);
         dataSource.setUsername(username);
--- SpringMVCssm/src/main/java/com/cqu/config/MyBatisConfig.java
+++ SpringMVCssm/src/main/java/com/cqu/config/MyBatisConfig.java
@@ -2,19 +2,24 @@ package com.cqu.config;
 
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
 
 public class MyBatisConfig {
-    public SqlSessionFactoryBean sqlSessionFactoryBean(DataSource dataSource){
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource){
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
         factoryBean.setDataSource(dataSource);
         factoryBean.setTypeAliasesPackage("com.cqu.domain");
         return factoryBean;
     }
+
+    @Bean
     public MapperScannerConfigurer mapperScannerConfigurer(){
         MapperScannerConfigurer msc = new MapperScannerConfigurer();
         msc.setBasePackage("com.cqu.dao");
-        return  msc;
+        return msc;
     }
 }
