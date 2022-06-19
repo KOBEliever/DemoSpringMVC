283139fa5ceddd55d6483c5ce01dd69c4df9d657
一 丁 <970102437@qq.com>
2022-06-19 22:14:59 +0800

    接口测试（报错无法创建Bean）

--- SpringMVCssm/src/main/java/com/cqu/config/JdbcConfig.java
+++ SpringMVCssm/src/main/java/com/cqu/config/JdbcConfig.java
@@ -3,16 +3,18 @@ package com.cqu.config;
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 public class JdbcConfig {
-    @Value("${jdbc.driver}")
+    @Value("$com.mysql.cj.jdbc.Driver")
     private String driver;
-    @Value("${jdbc.url}")
+    @Value("jdbc:mysql://localhost:3306/ssm_db")
     private String url;
-    @Value("${jdbc.username}")
+    @Value("root")
     private String username;
-    @Value("${jdbc.password}")
+    @Value("010609dy")
     private String password;
     @Bean
     public DataSource dataSource(){
@@ -22,5 +24,11 @@ public class JdbcConfig {
         dataSource.setUsername(username);
         dataSource.setPassword(password);
         return dataSource;
+    }
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource){
+        DataSourceTransactionManager ds = new DataSourceTransactionManager();
+        ds.setDataSource(dataSource);
+        return ds;
     }
 }
